522d2ee7051444c64bc08bf9d1f6e62467f1a731
AlexF99 <alexandreopf@gmail.com>
2023-02-07 15:16:57 -0300

    started firstFitMalloc translation to asm

--- api.s
+++ api.s
@@ -45,14 +45,14 @@ setNext:
 
     # "if"
     cmp %rcx, validAddress
-    jl fim_if
+    jl sn_fim_if
     cmp %rcx, heapBegin
-    jge fim_if
+    jge sn_fim_if
 
     # *(blockBegin + blockSize) = 0 
     movq $0, (%rcx)
 
-    # validAdress = blockBegin + blockSize
+    # validAddress = blockBegin + blockSize
     movq %rcx, validAddress
     
     # *(blockBegin + blockSize + 1) = totalAllocated - blockSize;
@@ -60,8 +60,8 @@ setNext:
     subq %rax, %rsi
     movq %rsi, (%rcx)
 
-    fim_if:
-    addq $32, %rsp
+    sn_fim_if:
+    addq $24, %rsp
     popq %rbp
     ret
 
@@ -74,6 +74,91 @@ bestFitMalloc:
 firstFitMalloc:
     pushq %rbp
     movq %rsp, %rbp
+
+    subq $40, %rsp
+
+    movq %rdi, %rdx # salva parametro (num_bytes) em rdx
+
+    # Chama a brk - currentTop = sbrk(0)
+    movq $0, %rdi
+    movq $12, %rax
+    syscall
+    movq %rax, -8(%rbp)
+
+    # *p = heapBegin
+    movq heapBegin, %rbx
+    movq %rbx, -16(%rbp)
+
+    # m = 1
+    movq $1, -24(%rbp)
+
+    # %rax = num_bytes + 16
+    movq %rdx, %rax
+    addq $16, %rax
+
+    # %rbx = 4096 * m
+    # while (rax > rbx) ...
+    movq -24(%rbp), %rcx
+    movq %rcx, %rbx
+    ffm_while_m:
+    imul $4096, %rbx
+    cmp %rax, %rbx
+    jle ffm_fim_while_m
+    addq $1, %rcx
+    movq %rcx, %rbx
+    jmp ffm_while_m
+    ffm_fim_while_m:
+    movq %rbx, -32(%rbp) # totalBytes = 4096*m
+    
+    movq -16(%rbp), %rax # rax = p
+
+    # p < (int*) validAdress
+    ffm_while_va:
+    cmp %rax, validAddress
+    jge ffm_fim_while_va
+    
+    # (if *p == 0 && *(p + 1) >= num_bytes)
+    movq $0, %r8
+    cmp (%rax), %r8
+    jne ffm_fim_if_0
+    movq %rax, %rbx
+    addq $8, %rbx
+    cmp (%rbx), %rdx
+    jl ffm_fim_if_0
+
+    movq $1, (%rax)
+
+    # (if ((int *)validAdress <= p))
+    cmp validAddress, %rax
+    jg ffm_fim_if_1
+
+    movq (%rbx), %r8
+    movq %r8, -40(%rbp)
+    movq %rdx, (%rbx)
+
+    movq %rax, %rdi
+    movq -40(%rbp), %rsi
+    call setNext
+
+    ffm_fim_if_1:
+    # return p + 2;
+    jmp ffm_ret
+
+    ffm_fim_if_0:
+    movq (%rbx), %rbx
+    addq $2, %rbx
+    addq %rbx, %rax
+    jmp ffm_while_va
+
+    ffm_fim_while_va:
+    movq %rax, -16(%rbp)
+
+    # TODO: Nenhuma memória foi alocada ainda, ou falta memória
+    
+    ffm_ret:
+    movq -16(%rbp), %rax
+    addq  $16, %rax
+    addq $40, %rsp
     popq %rbp
     ret
 
